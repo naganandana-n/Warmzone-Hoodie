51851cefffbbe3dba2f4824271fadac1781f78b4
naganandana <naganandana17@gmail.com>
2025-03-27 09:37:46 +0400

    ui changes test

--- ui/launcher.py
+++ ui/launcher.py
@@ -1,13 +1,13 @@
 import subprocess
 import os
+import sys
 
+venv_python = os.path.abspath(sys.executable)  # This gets path to current venv's python
 web_py_path = os.path.abspath("web.py")
-print(f"Running web.py from: {web_py_path}")
 
 powershell_command = f'''
-Start-Process powershell -ArgumentList "-NoProfile -WindowStyle Hidden -Command python '{web_py_path}'" -WindowStyle Hidden
+Start-Process powershell -ArgumentList "-NoProfile -Command '{venv_python}' '{web_py_path}'"
 '''
 
-print("Launching PowerShell command...")
+print("Launching with venv interpreter:", venv_python)
 subprocess.run(["powershell", "-Command", powershell_command], shell=True)
-print("Command executed.")
