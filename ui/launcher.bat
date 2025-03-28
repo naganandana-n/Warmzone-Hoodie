07e5008521a4a9d9f8634b677446a49cdb087261
naganandana <naganandana17@gmail.com>
2025-03-28 14:53:09 +0400

    changed launcher.bat to hide terminals

--- ui/launcher.bat
+++ ui/launcher.bat
@@ -1,11 +1,10 @@
-
 @echo off
 cd /d "%~dp0"
 
 echo Launching Nana Warmzone Controller frontend...
-start "" venv\Scripts\python.exe web.py
+call venv\Scripts\python.exe web.py
 
 echo Launching backend processor...
-start "" venv\Scripts\python.exe backend.py
+call venv\Scripts\python.exe backend.py
 
 exit
